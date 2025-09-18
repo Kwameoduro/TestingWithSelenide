1f405bd8530ceff96a86c474c2286c97fa9b5ee0
Kwame Oduro <odurokwameee@gmail.com>
2025-09-18 23:34:55 +0000

    test: add junit listener for test lifecycle and reporting

---
+++ src/test/java/com/selenide/listener/JUnitlistener.java
@@ -0,0 +1,31 @@
+package com.selenide.listener;
+
+
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.TestWatcher;
+
+import java.util.Optional;
+
+public class JUnitlistener implements TestWatcher {
+
+    @Override
+    public void testSuccessful(ExtensionContext context) {
+        TestListener.onTestSuccess(context.getDisplayName());
+    }
+
+    @Override
+    public void testFailed(ExtensionContext context, Throwable cause) {
+        TestListener.onTestFailure(context.getDisplayName(), cause);
+    }
+
+    @Override
+    public void testDisabled(ExtensionContext context, Optional<String> reason) {
+        TestListener.onTestSkipped(context.getDisplayName(), reason.orElse("No reason"));
+    }
+
+    @Override
+    public void testAborted(ExtensionContext context, Throwable cause) {
+        TestListener.onTestFailure(context.getDisplayName(), cause);
+    }
+}
